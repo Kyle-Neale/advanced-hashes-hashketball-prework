926e9acfd7b7abf21fab4c3cdfd14acaea62b6d2
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-05 17:49:55 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,16 +129,9 @@ end
 
 def num_points_scored(player_name)
   player_points = 0
-  game_hash.each do |team, team_data|
-   team_data[:players].each do |player|
-     
-    # if attribute == :players
-    #   data.each do |player|
-      if player[:player_name] == player_name
-        player_points = player[:points].to_i
-          end
-    #     end
-      end
+  all_players.each do |player|   
+    if player == player_name
+      player_points = player[:points].to_i
     end
   end
   player_points
